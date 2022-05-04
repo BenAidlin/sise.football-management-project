1edd0562bbb01550b72f09c5feba413e862977f3
BenAidlin <ben.aidlin@gmail.com>
2022-05-04 16:43:31 +0300

    add test template and empty tests for login sign in

---
+++ src/UnitTests/DataAccess/UserDaoTest.java
@@ -0,0 +1,40 @@
+package UnitTests.DataAccess;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
